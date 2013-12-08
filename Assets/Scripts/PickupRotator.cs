f6a81bb88d2e247c8b2f04aeab7ed3e56f4a40d6
Nicholas Rasband <nrasband@zynga.com>
2013-12-07 23:12:28 -0800

    Initial Commit. Adding partially finished tutorial.

---
+++ Assets/Scripts/PickupRotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupRotator : MonoBehaviour
+{
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Rotate the cube
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float Speed;
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+		rigidbody.AddForce(movement * Speed * Time.deltaTime);
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Pickup")
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
+}
